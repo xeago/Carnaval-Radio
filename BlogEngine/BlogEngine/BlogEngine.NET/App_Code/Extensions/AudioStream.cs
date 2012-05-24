9de6593e2d26062f6643af7b9af42a9f6f0e7407
Ferdinant <LarsBrand@gmail.com>
2012-05-24 15:08:41 +0200

    added audio stream functions

---
+++ BlogEngine/BlogEngine/BlogEngine.NET/App_Code/Extensions/AudioStream.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using BlogEngine.Core;
+using BlogEngine.Core.Web.Controls;
+using BlogEngine.Core.Web.Extensions;
+
+/// <summary>
+/// Summary description for AudioStream
+/// </summary>
+[Extension("Extension used to give AudioStreams","1.0","Nick & Lars")]
+public class AudioStream 
+{
+	public AudioStream()
+    {
+        //Comment.Serving += new EventHandler<ServingEventArgs>(Post_CommentServing);
+        ExtensionSettings settings = new ExtensionSettings("AudioStream");
+
+        settings.AddParameter("Server", "Server.", 254, false,true,ParameterType.String );
+        settings.AddValue("Server", "Http://VoorbeeldServer.nl");
+
+        settings.AddParameter("Quality","Qualiteit. Hoog/Laag.",10,true,false,ParameterType.String);
+        settings.AddValue("Quality", "quality");
+
+        settings.AddParameter("Priority","Prioriteit, Geeft aan welke stream de standaard stream is. Het Laagste getal zal gebruikt worden.",2,true,false,ParameterType.Integer);
+        settings.AddValue("Priority", 10);
+       
+        ExtensionManager.ImportSettings(settings);
+        
+	
+	}
+
+
+   
+        
+
+}
---
+++ BlogEngine/BlogEngine/BlogEngine.NET/App_Code/WebService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using BlogEngine.Core;
+using BlogEngine.Core.Web.Extensions;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "/BlogEngine/AudioStreams")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+[ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public AudioStreamObject GetAudioStreams()
+    {
+        ExtensionSettings settings = new ExtensionSettings("AudioStream");
+        DataTable table = settings.GetDataTable();
+
+        List<AudioStreamObject> audioStreams = new List<AudioStreamObject>();
+        foreach (DataRow row in table.Rows)
+        {
+            AudioStreamObject s = new AudioStreamObject();
+            s.Server = (string)row["Server"];
+            s.Quality = (string)row["Quality"];
+            s.Priority = ((int) row["Priority"]).ToString();           
+            audioStreams.Add(s);
+        }
+
+        AudioStreamObject output = audioStreams.Count == 0 ? audioStreams[audioStreams.Count-1] : new AudioStreamObject("http://TestServer.nl","Low","10");
+
+        foreach (AudioStreamObject audioStreamObject in audioStreams)
+        {
+            if (Convert.ToInt32(output.Priority) < Convert.ToInt32(audioStreamObject.Priority))
+            {
+                output = audioStreamObject;
+            }
+        }
+        return output;
+    }
+
+}
+
+public class AudioStreamObject
+{
+    public AudioStreamObject ()
+    {
+        
+    }
+    public AudioStreamObject(string server, string quality, string priority)
+    {
+        Server = server;
+        Quality = quality;
+        Priority = priority;
+        }
+    public string Server;
+    public string Quality;
+    public string Priority;
+
+}
