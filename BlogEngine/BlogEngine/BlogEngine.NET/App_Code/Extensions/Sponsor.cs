77396866e1f3cf90018baed69a7a4a0c5444b9f3
Nick Nijenhuis <nicknijenhuis@gmail.com>
2012-05-25 14:50:51 +0200

    Sponsoren bijbouwen (Helaas alles als string opslaan omdat de rest niet goed wordt ondersteund.

---
+++ BlogEngine/BlogEngine/BlogEngine.NET/App_Code/Extensions/Sponsor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using BlogEngine.Core;
+using BlogEngine.Core.Web.Controls;
+using BlogEngine.Core.Web.Extensions;
+using Resources;
+
+/// <summary>
+/// Summary description for AudioStream
+/// </summary>
+[Extension("Extension used to set sponsors", "1.0", "Nick")]
+public class Sponsor
+{
+    public Sponsor()
+    {
+        //Comment.Serving += new EventHandler<ServingEventArgs>(Post_CommentServing);
+        var settings = new ExtensionSettings("Sponsor");
+
+        settings.AddParameter("Name", labels.name, 150, true, true, ParameterType.String);
+        settings.AddParameter("URL", labels.website, 255, false, false, ParameterType.String);
+
+        #region Type Bools
+
+        //Sponsor Page Bools
+        settings.AddParameter("SponsorPage_Hoofdsponsor", labels.HoofdSponsor, false, false, ParameterType.String);
+        settings.AddParameter("SponsorPage_Sponsor", labels.Sponsor, false, false, ParameterType.String);
+        settings.AddParameter("SponsorPage_Subsponsor", labels.SubSponsor, false, false, ParameterType.String);
+        settings.AddParameter("SponsorPage_ClubVan50", labels.ClubVan50, false, false, ParameterType.String);
+        settings.AddParameter("SponsorPage_VriendenVanCR", labels.VriendenVanCR, false, false, ParameterType.String);
+
+        //Player Bools
+        settings.AddParameter("Player_Switch", labels.Switch, false, false, ParameterType.String);
+        settings.AddParameter("Player_Solid", labels.Solid, false, false, ParameterType.String);
+
+        //Widget
+        settings.AddParameter("Widget_Switch", labels.Switch, false, false, ParameterType.String);
+
+        //Mobiele app
+        settings.AddParameter("Mobile", labels.Switch, false, false, ParameterType.String);
+        #endregion
+
+        settings.AddParameter("Mobile_Frequency", labels.Switch, false, false, ParameterType.String);
+
+        settings.AddParameter("Logo", labels.sponsorlogo, 255, false, false, ParameterType.String);
+
+        settings.AddParameter("Description", labels.description, int.MaxValue, false, false, ParameterType.String);
+
+        settings.AddParameter("CreationDate", labels.setPublishDate, false, false, ParameterType.String);
+        settings.AddParameter("EndDate", labels.endDate, false, false, ParameterType.String);
+        settings.AddParameter("Active", labels.active, false, false, ParameterType.String);
+
+        ExtensionManager.ImportSettings(settings);
+    }
+}
+
+
+
+
+
---
+++ BlogEngine/BlogEngine/BlogEngine.NET/admin/Sponsoren/Sponsoren.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/admin/admin.master" AutoEventWireup="true"
+    CodeFile="Sponsoren.aspx.cs" ValidateRequest="false" Inherits="Admin.Sponsoren.SponsorenPage"
+    Title="Sponsor toevoegen" %>
+<%@ Register src="Menu.ascx" tagname="TabMenu" tagprefix="menu" %>
+
+<%@ Register Src="~/admin/htmlEditor.ascx" TagPrefix="Blog" TagName="TextEditor" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphAdmin" runat="Server">
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $(".tableToolBox a").click(function () {
+                $(".tableToolBox a").removeClass("current");
+                $(this).addClass("current");
+            });
+        });
+    </script>
+    <div class="content-box-outer">
+		<div class="content-box-right">
+			<menu:TabMenu ID="TabMenu" runat="server" />
+		</div>
+		<div class="content-box-left">
+            <h1><%=Resources.labels.sponsoren %></h1>
+            <div class="tableToolBox">
+               
+            </div>
+            <div id="Container"></div>
+        </div>
+    </div>
+</asp:Content>
