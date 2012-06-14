041828643db738fdc5296488772b5f7b33b4a4d8
Nick Nijenhuis <nicknijenhuis@gmail.com>
2012-06-14 22:04:09 +0200

    sponsoren bovenin functionaliteit

---
+++ BlogEngine/BlogEngine/BlogEngine.NET/App_Code/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace ExtensionMethods
+{
+    /// <summary>
+    /// Summary description for ExtensionMethods
+    /// </summary>
+    public static class MyExtensions
+    {
+        public static string ToUrlString(this string s)
+        {
+            return s.ToLower().StartsWith(@"http://") ? s : ("http://" + s);
+        }
+    }
+}
