cc0aa3c9964b42554ceab2d7d7ebea9aa9ac9a39
Twan Wolthof <twan@xeago.nl>
2012-06-22 13:21:52 +0200

    Add simple method to retrieve and output recent songs

---
+++ BlogEngine/BlogEngine/BlogEngine.NET/App_Code/RecentSongsService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Net;
+using System.Text;
+using BlogEngine.Core.Json;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class RecentSongsService : System.Web.Services.WebService {
+
+    public RecentSongsService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public object RecentSongs() {
+        var l =recentList();
+        var jss = new System.Web.Script.Serialization.JavaScriptSerializer();
+
+        var lijstje = new List<dynamic>();
+        foreach (var item in l)
+        {
+            lijstje.Add(new { song = item });
+        }
+
+        return lijstje;
+    }
+
+    private static List<string> recentList()
+    {
+        WebClient c = new WebClient();
+        c.Headers.Add(@"User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
+        var a = c.DownloadData("http://50.7.241.10:8021/played.html");
+        var s = Encoding.UTF8.GetString(a);
+        var stringa = new string[] { @"<br><table border=0 cellpadding=2 cellspacing=2><tr><td>Played @</td><td><b>Song Title</b></td></tr><tr>" };
+
+        var stuff = s.Split(stringa, StringSplitOptions.RemoveEmptyEntries)[1];
+        var aftertd = stuff.Split(new string[] { @"</td><td>" }, StringSplitOptions.RemoveEmptyEntries);
+
+        var lijstje = new List<string>();
+        var nowplaying = aftertd[1].Split(new String[] { @"<td><b" }, StringSplitOptions.RemoveEmptyEntries)[0];
+        lijstje.Add(nowplaying);
+        for (int i = 2; i < aftertd.Length; i++)
+        {
+            lijstje.Add(aftertd[i].Split(new string[] { @"</tr>" }, StringSplitOptions.RemoveEmptyEntries)[0]);
+        }
+        return lijstje;
+    }
+}
